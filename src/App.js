99951791d8297f2e5472eef15f19c729d974223c
Bharathan <bharathan655@gmail.com>
2024-06-19 12:04:04 +0530

    data fetched successfully and basic model created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import MovieBox from './MovieBox';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=e7302e09244b40ccd8f190889d0f45d8";
 function App() {
+
+  const [movies,setMovies] = useState([]);
+
+  useEffect(()=>{
+    fetch(API_URL)
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data)
+      setMovies(data.results)
+    })
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="grid">
+        {movies.map((movieReq)=><MovieBox key={movieReq.id} {...movieReq}/>)}
+      </div>       
     </div>
   );
 }
